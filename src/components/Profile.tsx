86ad197829c34b2d70ae821791d514e457eb8d85
Rodrigo Kalil <rkbuarque@gmail.com>
2021-09-14 09:32:19 -0300

    fist commit pushing project to repo

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://image.shutterstock.com/image-vector/male-silhouette-avatar-profile-picture-600w-199246382.jpg" alt="Rodrigo Kalil"/>
+            <div>
+                <strong>Rodrigo Kalil</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    )
+}
